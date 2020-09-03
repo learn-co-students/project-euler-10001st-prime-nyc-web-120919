b973c6c854e466a361280e7107f4b571063a5716
flatironschool <danliu277@gmail.com>
2020-09-03 17:21:24 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,27 @@
 # Implement your procedural solution here!
+def is_prime?(number)
+    if number == 0 || number == 1
+        return false
+    end
+    result = true
+    limit = Math.sqrt(number).round
+    (2..limit).each do |i| 
+        if (number % i) == 0
+            result = false
+            break
+        end
+    end
+    return result
+end
+
+def prime_number_for(nth_element)
+    count = 0
+    number = 0
+    while count != nth_element
+        number += 1
+        if is_prime?(number)
+            count += 1 
+        end
+    end
+    return number
+end
